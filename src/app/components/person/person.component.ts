4d086fcc30b2b1e18c8a470ccee5e1401e82de36
Andrew Corsini <sudz@Andrews-MacBook-Air.local>
2023-04-14 03:02:35 -0500

    fixed json-server

--- src/app/components/person/person.component.ts
+++ src/app/components/person/person.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { Person } from 'src/app/models/Person';
 
 @Component({
@@ -9,6 +9,12 @@ import { Person } from 'src/app/models/Person';
 export class PersonComponent {
   @Input() person!: Person;
   @Output() deleteEvent: EventEmitter<Person> = new EventEmitter();
+  avatarUrl: String = "https://api.dicebear.com/6.x/bottts/svg";
+
+  ngOnInit(): void {
+    this.avatarUrl += `?seed=${this.person.firstName+this.person.lastName}`;
+    console.log('avatarUrl: ', this.avatarUrl);
+  }
 
   onDelete() {
     this.deleteEvent.emit(this.person)
